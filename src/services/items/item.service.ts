d3d6bcd3a2393f20790b7f471b6701ee959cad08
BNigmatov <baha@online.uz>
2024-09-08 18:18:50 +0500

    feat(api): obtain items from the remote API
    ```
    Нужно обратиться к списку предметов skinport (https://docs.skinport.com/#items) и отобразить массив объектов, где помимо прочего должны быть указаны две минимальные цены на предмет (одна цена — tradable, другая — нет) параметры app_id и currency - default, базу здесь использовать не нужно, в эндпоинте необходимо использовать кеширование
    ```
    > not implemeted yet `в эндпоинте необходимо использовать кеширование`

--- src/services/items/item.service.ts
+++ src/services/items/item.service.ts
@@ -2,6 +2,27 @@ import { Item } from './item.type';
 
 export class ItemService {
     async getAll(): Promise<Item[]> {
-        throw new Error('[ItemService]: "getAll" method is not implemented');
+        // ! Warning: не понятно требование
+        // > где помимо прочего должны быть указаны две минимальные цены на предмет (одна цена — tradable, другая — нет)
+        // * Что значит *tradable цена* ?
+        // возможно имелись ввиду какие-то танцы с *tradable* `in: path` параметром внешнего API
+        // smth like
+        /*
+         const [a, b] = Promise.all([this.itemsFromRemoteAPI(), this.itemsFromRemoteAPI(true)]);
+         и дальше агрегация из 2-х массивов(немаленьких), чтобы не попасть в `O(n2)`
+         но лучше писать так, чтобы не приходилось гадать
+        */
+        return this.itemsFromRemoteAPI(true);
+    }
+    private async itemsFromRemoteAPI(tradableOnly = false): Promise<Item[]> {
+        /*
+        If true, it shows only tradable items on the market (Default false)
+        */
+        return fetch(`https://api.skinport.com/v1/items?tradable=${tradableOnly}`, {
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+            .then((resp) => resp.json() as Promise<Item[]>);
     }
 }
