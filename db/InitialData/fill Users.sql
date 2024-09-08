9118a76c083e9ca5575906dba26d75d2e797cb28
BNigmatov <baha@online.uz>
2024-09-08 19:32:50 +0500

    feat(db): create database

---
+++ db/InitialData/fill Users.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT dbo.Users ON;
+
+IF(NOT EXISTS(SELECT * FROM dbo.Users WHERE id = 1)) BEGIN
+	INSERT dbo.Users(id, name) VALUES (1, 'test-user');
+END
+
+SET IDENTITY_INSERT dbo.Users OFF;
+
+SELECT * FROM dbo.Users
---
+++ db/Tables/dbo.Users.sql
@@ -0,0 +1,14 @@
+﻿-- =========================================
+-- Create table dbo.Users
+-- =========================================
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL
+	DROP TABLE dbo.Users
+GO
+
+CREATE TABLE dbo.Users (
+	id INT NOT NULL IDENTITY(1, 1) NOT FOR REPLICATION
+	, CONSTRAINT PK_Users_id PRIMARY KEY CLUSTERED (id)
+	, name VARCHAR(100) NOT NULL
+	, balance NUMERIC(25,2) NOT NULL CONSTRAINT DF_Users_Balance DEFAULT 0
+	)
+GO
