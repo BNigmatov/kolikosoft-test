5e39cec24280d076f150a45816b8d616c2eef0dc
BNigmatov <baha@online.uz>
2024-09-08 16:31:22 +0500

    feat(app): add helper method to the router

--- src/routers/base.router.ts
+++ src/routers/base.router.ts
@@ -1,5 +1,35 @@
 import { IRouter, IHandler, IApplication } from '../types/common.type.js';
 import { Router } from 'express';
+import { ERROR_EMPTYPARAM, ERROR_INVALIDTYPE, RouterError } from '../types/error.type.js';
+
+export function extractParam<T>(req: Record<string, unknown>, name: string, type: string, required = true): T {
+    const { [name]: value } = req;
+    if (typeof value === 'undefined' && required) {
+        throw new RouterError(`Parameter ${name} is undefined`, ERROR_EMPTYPARAM, 400);
+    }
+    let isValidType = true;
+    let typedValue: unknown = value;
+    switch (type) {
+        case 'number':
+            typedValue = Number(value);
+            isValidType = !isNaN(Number(value));
+            break;
+        case 'boolean':
+            typedValue = (value === 'false') ? false : !!value;
+            break;
+        case 'string':
+            isValidType = true;
+            break;
+        default:
+            isValidType = false;
+    }
+    if (isValidType) {
+        return typedValue as T;
+    }
+    else {
+        throw new RouterError(`parameter ${name} is not a ${type}`, ERROR_INVALIDTYPE, 400);
+    }
+}
 
 export class BaseRouter<T = Record<string, never>> implements IRouter {
     protected _router: Router;
